6e817dd20a636b68f091a070936d1f3773f1276f
jd601 <jd601@kent.ac.uk>
2017-03-17 08:46:00 +0000

    code clean added icosn

--- index.php
+++ index.php
@@ -15,6 +15,26 @@
 <html>
 <head>
 <title>Bookit</title>
+
+		<!--this content is from the uni kent website, there is no point of wrting it if we have acsess to it view-source:https://www.kent.ac.uk/ for refrence -->
+		<link rel="apple-touch-icon" sizes="57x57" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/favicon-194x194.png" sizes="194x194">
+		<link rel="icon" type="image/png" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/manifest.json">
+		<link rel="mask-icon" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/safari-pinned-tab.svg" color="#003882">
+		<link rel="shortcut icon" href="https://static.kent.ac.uk/pantheon/kent-theme-assets/assets/images/favicons/favicon.ico">
+		<!--end of stuff taken from the uni kent website-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0 user-scalable=0"/> 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> <!--thats right jquery is being used here, you better use them libarys rather than doing it the long way-->
 <script src="ajax/Scripts/Load_items.js"></script> <!--want to load them items on the page? this does it-->
